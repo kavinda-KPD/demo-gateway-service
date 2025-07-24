02484577cf4ef05b6a7c1648c66de456d37efd34
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-24 16:25:22 +0530

    update packages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { USER_ACCOUNT, USER_ACCOUNT_SERVICE_RABBIT_MQ } from './constants';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    @Inject(USER_ACCOUNT_SERVICE_RABBIT_MQ)
+    private readonly userAccountService: ClientProxy,
+  ) {}
+
+  @Get('user-account')
+  async getUserAccount(): Promise<string> {
+    return firstValueFrom(
+      this.userAccountService.send(USER_ACCOUNT.GET_USER_ACCOUNT, {
+        name: 'John Doe',
+      }),
+    );
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  USER_ACCOUNT_QUEUE,
+  USER_ACCOUNT_SERVICE_RABBIT_MQ,
+} from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: USER_ACCOUNT_SERVICE_RABBIT_MQ,
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: USER_ACCOUNT_QUEUE,
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
