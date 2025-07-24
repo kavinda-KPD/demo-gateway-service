6a541cc167d0b40cddbd5f5b8d272db3848b406b
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-07-24 17:33:49 +0530

    create the docker composer

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,11 @@ import {
         name: USER_ACCOUNT_SERVICE_RABBIT_MQ,
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://guest:guest@localhost:5672'],
+          // urls: ['amqp://guest:guest@localhost:5672'],
+          urls: [
+            // `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASS}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
+            `${process.env.RABBITMQ_URL}`,
+          ],
           queue: USER_ACCOUNT_QUEUE,
           queueOptions: {
             durable: true,
